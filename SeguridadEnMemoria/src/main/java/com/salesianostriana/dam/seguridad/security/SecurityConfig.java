ab932ccff6f98f015c308b2e9194d66078fa23df
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2023-05-04 15:34:54 +0200

    Se arregal seguridad. Fin documentación

--- SeguridadEnMemoria/src/main/java/com/salesianostriana/dam/seguridad/security/SecurityConfig.java
+++ SeguridadEnMemoria/src/main/java/com/salesianostriana/dam/seguridad/security/SecurityConfig.java
@@ -3,9 +3,14 @@ package com.salesianostriana.dam.seguridad.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,27 +19,46 @@ public class SecurityConfig {
 	
 	@Bean
 	public AuthenticationManager 
-			authenticationManager(HttpSecurity http) {
+			authenticationManager(HttpSecurity http) throws Exception {
 		
 		AuthenticationManagerBuilder authBuilder =
 				http.getSharedObject(AuthenticationManagerBuilder.class);
 		
-		/*return authBuilder
-			.inMemoryAuthentication()
-			.passwordEncoder(PasswordEncoderFactories
-					.createDelegatingPasswordEncoder())
-			.withUser("admin")
-			.password("{noop}admin")
-			.roles("ADMIN")
-			.
-		*/
-		return authBuilder.authenticationProvider(null)
+		return authBuilder
+			.authenticationProvider(daoAuthenticationProvider())
+			.build();
+		
 		
 	}
 	
 	@Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.builder()
+        		.username("admin")
+        		.password("{noop}admin")
+        		.roles("ADMIN")
+            .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+	
+	@Bean 
+	public DaoAuthenticationProvider daoAuthenticationProvider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setUserDetailsService(userDetailsService());
+		provider.setPasswordEncoder(PasswordEncoderFactories.createDelegatingPasswordEncoder());
+		return provider;
+	}
+	
+	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		
+		http
+		.authorizeRequests()
+			.antMatchers("/css/**","/js/**","/webjars/**").permitAll()
+			.anyRequest().authenticated()
+			.and()
+		.formLogin()
+			.loginPage("/login")
+			.permitAll();
 		
 		return http.build();
 	}
